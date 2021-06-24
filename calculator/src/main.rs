5081b8b97316eae11badb4767a5007babbdb685b
jacremer <jacremer@live.com>
2021-06-24 00:02:15 -0700

    calculator finished

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -6,7 +6,21 @@ fn main() {
     }
 }
 
-fn calculate<'a>(n1: f32, n2: f32, op: char) -> Result<f32, &'a str> {
+fn prompt() {
+    println!("Pick an operator [+-*/]: ");
+    let op: char = read_op();
+    println!("Pick the first number: ");
+    let n1 = read_num();
+    println!("Pick the second number: ");
+    let n2 = read_num();
+
+    match calculate(op, n1, n2) {
+        Ok(n) => println!("{} {} {} = {}", n1, op, n2, n),
+        Err(e) => panic!(e),
+    }
+}
+
+fn calculate<'a>(op: char, n1: f32, n2: f32) -> Result<f32, &'a str> {
     match op {
         '+' => Ok(n1 + n2),
         '-' => Ok(n1 - n2),
@@ -16,11 +30,17 @@ fn calculate<'a>(n1: f32, n2: f32, op: char) -> Result<f32, &'a str> {
     }
 }
 
-fn prompt() {}
+fn read_num() -> f32 {
+    let mut res = String::new();
+    read(&mut res);
+    res.trim().parse().unwrap()
+}
 
-fn read_num<'a>() -> Result<f32, &'a str> {}
-
-fn read_op<'a>() -> Result<char, &'a str> {}
+fn read_op() -> char {
+    let mut res = String::new();
+    read(&mut res);
+    res.trim().chars().next().unwrap()
+}
 
 fn read(input: &mut String) {
     stdout().flush().expect("failed to flush");
