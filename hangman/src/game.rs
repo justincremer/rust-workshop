77f5027bc38d08a5633f700b0df7c7b7be8f3e98
justincremer <jacremer@live.com>
2021-06-25 12:32:52 -0700

    updates hangman struct

--- hangman/src/game.rs
+++ hangman/src/game.rs
@@ -1,49 +1,45 @@
-use regex::Regex;
-
-pub struct Session<'a> {
-    answer: String,
-    letters_guessed: String,
-    solution: &'a str,
-    guesses_left: u8,
+pub struct Session {
+    pub hidden: String,
+    pub answer: String,
+    pub letters_guessed: String,
+    pub guesses_left: u8,
 }
 
-impl Session<'_> {
-    pub fn new(solution: &str, guesses: u8) -> Session {
-        let re = Regex::new(r"[A-Za-z]").unwrap();
+impl Session {
+    pub fn new(answer: &str, guesses: u8) -> Session {
+        let answer = &answer.to_lowercase();
+        let hidden = String::from(answer)
+            .chars()
+            .map(|c| match c {
+                _ => '*',
+            })
+            .collect();
+
         Session {
-            answer: re.replace_all(solution, "*"),
-            solution: solution,
+            hidden: hidden,
+            answer: answer.to_string(),
+            letters_guessed: String::new(),
             guesses_left: guesses,
         }
     }
 
-    fn guess(&self, l: char) -> Option<&str> {
-        if !self.letters_guessed.contains(l) {
-            self.letters_guessed.add(l);
-            match find_all(self.solution, l, None) {
-                None => Some("{} is not in the solution"),
-                Some(x) => Some(format!(
-                    "There are {} instances of the letter {}",
-                    x.len(),
-                    l
-                )),
-            }
-        }
-        None
-    }
-}
+    pub fn guess(&mut self, c: char) -> Result<(), &str> {
+        match self.letters_guessed.contains(c) {
+            true => Err("Cannot guess a letter twice"),
+            false => {
+                self.letters_guessed.push(c);
 
-fn find_all(s: &str, c: char, acc: Option<[i8]>) -> Option<[i8]> {
-    // s.chars().position(|c| c == l)
-
-    match s.find(c) {
-        None => acc,
-        Some(i) => {
-            acc.insert(i);
-            let size = s.len();
-            match i {
-                size => acc,
-                _ => find_all(s[i..], c, acc),
+                let mut i = 0;
+                loop {
+                    if self.answer.chars().nth(i).unwrap() == c {
+                        self.hidden.chars().nth(i).replace(c);
+                    }
+                    i += 1;
+                    if i == self.answer.len() {
+                        break;
+                    }
+                }
+                Ok(())
             }
         }
     }
--- hangman/src/main.rs
+++ hangman/src/main.rs
@@ -2,6 +2,9 @@ mod game;
 use game::Session;
 
 fn main() {
-    let ses = Session::new("Hello World", 5);
-    println!("Hello, world!");
+    let game = Session::new(&"Hello World", 5);
+    println!(
+        "{} {} {} {}",
+        game.answer, game.hidden, game.letters_guessed, game.guesses_left
+    )
 }
